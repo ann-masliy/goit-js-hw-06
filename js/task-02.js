1ad6b1f27cda0a7e0835ea0f62867cc4007774e2
ann-masliy <ms.masliy@ukr.net>
2022-09-14 17:42:05 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,15 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingrListEl = document.querySelector("#ingredients");
+const items = ingredients.map(ingr => {
+  const itemEl = document.createElement('li');
+  itemEl.textContent = ingr;
+  itemEl.className = "item";  
+  return itemEl;
+  
+});
+
+ingrListEl.append(...items);
+console.log(ingrListEl);
