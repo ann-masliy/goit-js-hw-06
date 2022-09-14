0ed5159bb34dbd8673e4684510aa34d2055f10fc
ann-masliy <ms.masliy@ukr.net>
2022-09-14 18:25:50 +0300

    adds task 3

--- js/task-03.js
+++ js/task-03.js
@@ -12,3 +12,9 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+const galleryListRef = document.querySelector('.gallery');
+const galList = images
+  .map((image) => `<li class="gallery__item"><img class="gallery__img" src="${image.url}" alt="${image.alt}"></li>`)
+  .join("");
+galleryListRef.insertAdjacentHTML('afterbegin', galList);
