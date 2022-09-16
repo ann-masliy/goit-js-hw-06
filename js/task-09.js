1a25ea2d8c6721c22fa50a920f6107cc9354f6cd
ann-masliy <ms.masliy@ukr.net>
2022-09-16 18:18:52 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const bodyEl = document.querySelector("body");
+const setColorEl = document.querySelector(".color");
+const btnClickEl = document.querySelector(".change-color");
+
+btnClickEl.addEventListener('click', event => {
+  let color = getRandomHexColor();
+  bodyEl.style.backgroundColor = color;
+  setColorEl.textContent = color;
+});
