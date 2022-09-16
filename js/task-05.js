734b90113c71287e63d88a2c3f472afe9ba6693e
ann-masliy <ms.masliy@ukr.net>
2022-09-16 16:09:46 +0300

    Update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -2,7 +2,7 @@ const inputEl = document.querySelector('#name-input');
 const outputEl = document.querySelector('#name-output');
 
 inputEl.addEventListener('input',  event => {
-	outputEl.textContent = event.target.value
+	outputEl.textContent = event.target.value.trim()
 
 	if (event.target.value === '') {
 		outputEl.textContent = 'Anonymous'
