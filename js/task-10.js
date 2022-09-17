95e495e8d6b7f0e379bb1f947ad9e84e24d416f6
ann-masliy <ms.masliy@ukr.net>
2022-09-17 09:19:05 +0300

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,28 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const inputEl = document.querySelector('input');
+const createBtnEl = document.querySelector('[data-create]');
+const destroyBtnEl = document.querySelector('[data-destroy]');
+const boxes = document.querySelector('#boxes');
+
+function createBoxes() {
+  const boxArray = [];
+  for (let i = 0; i <  Number(inputEl.value); i++) {
+    const newDiv = document.createElement('div');
+    newDiv.style.width = `${30 + 10 * i}px`;
+    newDiv.style.height = `${30 + 10 * i}px`;
+    newDiv.style.background = getRandomHexColor();
+    boxArray.push(newDiv);
+  }
+  boxes.append(...boxArray);
+}
+
+function destroyBoxes() {
+  boxes.innerHTML = "";
+  inputEl.value = "";
+}
+
+createBtnEl.addEventListener('click', createBoxes);
+destroyBtnEl.addEventListener('click', destroyBoxes);
